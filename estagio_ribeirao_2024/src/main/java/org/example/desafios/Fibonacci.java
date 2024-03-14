c30b16bec3f83578ad21db7104aaa6a3e32e806e
Vinicius De Souza <100710995+Viniciusouza19@users.noreply.github.com>
2024-03-14 12:13:48 -0300

    Update Fibonacci.java

--- estagio_ribeirao_2024/src/main/java/org/example/desafios/Fibonacci.java
+++ estagio_ribeirao_2024/src/main/java/org/example/desafios/Fibonacci.java
@@ -3,12 +3,31 @@ package org.example.desafios;
 public class Fibonacci {
     public static void main(String[] args) {
         int fiboRange = 15;
-        int a = 0, b = 1;
-        for (int i = 0; i < fiboRange; i++) {
+        System.out.print("Sequência de Fibonacci até o " + fiboRange + "º termo: ");
+        for (int i = 0, a = 0, b = 1; i < fiboRange; i++) {
             System.out.print(a + " ");
             int temp = a + b;
             a = b;
             b = temp;
         }
+        
+        int numero = 13;
+        if (verificarFibonacci(numero)) {
+            System.out.println("\nO número " + numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("\nO número " + numero + " NÃO pertence à sequência de Fibonacci.");
+        }
+    }
+    
+    public static boolean verificarFibonacci(int n) {
+        for (int a = 0, b = 1; a <= n; ) {
+            if (a == n) {
+                return true;
+            }
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return false;
     }
 }
